04b42954a5a1ae600a915623e5aff0ff39c9f306
Sara Mahía Gómez <saramahiagomez@gmail.com>
2024-07-22 15:37:03 +0200

    chore: create log in form html

--- frontend/src/app/component/LoginComponent/login.component.ts
+++ frontend/src/app/component/LoginComponent/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+    NgClass
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  handleSubmit() {
+
+  }
 
 }
