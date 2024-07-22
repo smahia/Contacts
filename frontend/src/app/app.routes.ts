e6db52d7e9b803abb46be480bd8557fb7d4232b1
Sara Mahía Gómez <saramahiagomez@gmail.com>
2024-07-22 15:17:40 +0200

    chore: create home and login components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./component/RegisterComponent/register.component";
+import {HomeComponent} from "./component/HomeComponent/home.component";
+import {LoginComponent} from "./component/LoginComponent/login.component";
 
 export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomeComponent
+  },
   {
     path: 'registration',
     title: 'Registration',
     component: RegisterComponent
+  },
+  {
+    path: 'login',
+    title: 'Log in',
+    component: LoginComponent
   }
 ];
