3f6b88f00a8a2c644fe08a0ed66619c4ead02c4c
Sara Mahía Gómez <saramahiagomez@gmail.com>
2024-08-01 16:03:11 +0200

    chore: add routes for ListDetailsComponent and ContactDetails component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {HomeComponent} from "./component/HomeComponent/home.component";
 import {LoginComponent} from "./component/LoginComponent/login.component";
 import {ListingComponent} from "./component/ListingComponent/listing.component";
 import {AuthGuard} from "./guard/auth.guard";
+import {ListDetailsComponent} from "./component/ListDetailsComponent/list-details.component";
+import {ContactDetailsComponent} from "./component/ContactDetailsComponent/contact-details.component";
 
 export const routes: Routes = [
   {
@@ -25,6 +27,16 @@ export const routes: Routes = [
     path: 'myLists',
     title: 'My lists',
     component: ListingComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'list/:id', // TODO: check if the id if not a number or doesn't exist with a Guard
+    component: ListDetailsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'contact/:id', // TODO: check if the id if not a number or doesn't exist with a Guard
+    component: ContactDetailsComponent,
     canActivate: [AuthGuard]
   }
 ];
