ecdd19e8a6d890c41661c87247489da43ce3310a
Sara Mahía Gómez <saramahiagomez@gmail.com>
2024-08-06 12:25:04 +0200

    chore: implement the add email feature

---
+++ frontend/src/app/component/AddEmailComponent/add-email.component.ts
@@ -0,0 +1,77 @@
+import {Component, Input} from '@angular/core';
+import {NewEmailRequest} from "../../request/NewEmailRequest";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {EmailService} from "../../service/email/email.service";
+import Swal from "sweetalert2";
+import {NgClass, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-add-email',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule,
+    NgClass
+  ],
+  templateUrl: './add-email.component.html',
+  styleUrl: './add-email.component.scss'
+})
+export class AddEmailComponent {
+
+  email: NewEmailRequest = new NewEmailRequest();
+  @Input() idContact: number = 0;
+
+  addEmailForm = new FormGroup(
+    {
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      type: new FormControl('', Validators.required)
+    }
+  );
+
+  constructor(private emailService: EmailService) {
+  }
+
+  handleSubmit() {
+
+    if (this.addEmailForm.valid) {
+
+      this.email.email = this.addEmailForm.value.email!;
+      this.email.type = this.addEmailForm.value.type!;
+
+
+      this.emailService.addEmail(this.idContact, this.email).subscribe(
+        {
+          next: value => {
+            Swal.fire({
+              title: "Email has been successfully added",
+              icon: "success"
+            }).then(
+              () => {
+                window.location.reload();
+              }
+            );
+          },
+          error: err => {
+            console.log(err);
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "Something went wrong!",
+            }).then(
+              () => {
+                window.location.reload();
+              }
+            )
+          }
+        }
+      );
+
+    }
+
+  }
+
+}
--- frontend/src/app/service/email/email.service.ts
+++ frontend/src/app/service/email/email.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NewEmailRequest} from "../../request/NewEmailRequest";
+import {Observable} from "rxjs";
+import {EmailResponse} from "../../response/EmailResponse";
+
+const baseUrl = "http://localhost:8080/emails/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addEmail(idContact: number, email: NewEmailRequest): Observable<EmailResponse> {
+    return this.http.post(baseUrl + "add/" + idContact, email);
+  }
 }
