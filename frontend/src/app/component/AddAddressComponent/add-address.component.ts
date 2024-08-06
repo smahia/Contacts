8afba5fbcabee9a14e54e6df1972757a6aa3f551
Sara Mahía Gómez <saramahiagomez@gmail.com>
2024-08-06 12:26:59 +0200

    chore: implement the add address feature

---
+++ frontend/src/app/component/AddAddressComponent/add-address.component.ts
@@ -0,0 +1,74 @@
+import {Component, Input} from '@angular/core';
+import {AddressService} from "../../service/address/address.service";
+import {NewAddressRequest} from "../../request/NewAddressRequest";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import Swal from "sweetalert2";
+import {NgClass, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-add-address',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule,
+    NgClass
+  ],
+  templateUrl: './add-address.component.html',
+  styleUrl: './add-address.component.scss'
+})
+export class AddAddressComponent {
+
+  address: NewAddressRequest = new NewAddressRequest();
+  @Input() idContact: number = 0;
+
+  addAddressForm = new FormGroup(
+    {
+      address: new FormControl('', Validators.required),
+      type: new FormControl('', Validators.required)
+    }
+  );
+
+  constructor(private addressService: AddressService) {
+  }
+
+  handleSubmit() {
+
+    if (this.addAddressForm.valid) {
+
+      this.address.address = this.addAddressForm.value.address!;
+      this.address.type = this.addAddressForm.value.type!;
+
+
+      this.addressService.addAddress(this.idContact, this.address).subscribe(
+        {
+          next: value => {
+            Swal.fire({
+              title: "Address has been successfully added",
+              icon: "success"
+            }).then(
+              () => {
+                window.location.reload();
+              }
+            );
+          },
+          error: err => {
+            console.log(err);
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "Something went wrong!",
+            }).then(
+              () => {
+                window.location.reload();
+              }
+            )
+          }
+        }
+      );
+
+    }
+
+  }
+
+}
--- frontend/src/app/service/address/address.service.ts
+++ frontend/src/app/service/address/address.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NewAddressRequest} from "../../request/NewAddressRequest";
+import {Observable} from "rxjs";
+import {AddressResponse} from "../../response/AddressResponse";
+
+const baseUrl = "http://localhost:8080/address/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addAddress(idContact: number, address: NewAddressRequest): Observable<AddressResponse> {
+    return this.http.post(baseUrl + "add/" + idContact, address);
+  }
 }
